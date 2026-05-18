3fe31bbb3449416e8658318eefb3f7aa9601f6b9
Abodi37 <abdullahaljamaan37@gmail.com>
2026-05-18 16:25:45 +0300

    ...

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+ [Header("Camera Settings")]
+    public float mouseSensetivity = 100f;
+    public Transform CamX;
+    float xRotation = 0f;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        float MouseX = Input.GetAxis("Mouse X") * mouseSensetivity;
+        float MouseY = Input.GetAxis("Mouse Y") * mouseSensetivity;
+
+        xRotation -= MouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        CamX.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        transform.Rotate(Vector3.up * MouseX);
     }
 }
--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -2,15 +2,79 @@ using UnityEngine;
 
 public class PlayerMovements : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public CharacterController myPlayer;
+
+    [Header("Movements")]
+    public float speed = 4f; 
+     public float currentSpeed = 8f;
+     public float gravity = -20f;
+
+     [Header("StaminaLogic")]
+    public float stamina = 100f;
+    public float maxStamina = 100f; 
+    public float drainRate = 20f; 
+    public float refillRate = 15f;
+    public bool canSprint = true;
+
+     [Header("CheckGround")]
+    public bool grounded;
+    public Vector3 velocity;
+    public LayerMask groundMask;
+    public float groundDistance = 1.1f;
+
+    [Header("Smooth Jump")]
+    public float jumpForce = 8f;
+    public float liftDuration = 0.1f;
+    public float liftTimer; 
+
     void Update()
     {
-        
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = (transform.right * Horizontal) + (transform.forward * Vertical); 
+
+        grounded = Physics.Raycast(transform.position, Vector3.down, groundDistance, groundMask);
+        Debug.DrawRay(transform.position, Vector3.down * groundDistance, grounded ? Color.green : Color.red);
+
+        bool isTryingToSprint = Input.GetKey(KeyCode.LeftShift) && Vertical > 0; 
+
+        if (isTryingToSprint && canSprint && stamina > 0)
+        {
+            move *= currentSpeed;
+            stamina -= drainRate * Time.deltaTime;
+            if (stamina <= 0) canSprint = false;
+        }
+        else
+        {
+            move *= speed; 
+            if (stamina < maxStamina)
+                stamina += refillRate * Time.deltaTime;
+
+            if (stamina >= 100f) canSprint = true;
+        }
+
+        if (grounded && velocity.y < 0)
+        {
+            velocity.y = -1f;
+        }
+
+        if (Input.GetButtonDown("Jump") && grounded == true)
+        {
+            liftTimer = liftDuration;
+        }
+
+        if (liftTimer > 0)
+        {
+            velocity.y += jumpForce * (liftTimer / liftDuration) * Time.deltaTime * 10f;
+            liftTimer -= Time.deltaTime;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        myPlayer.Move(velocity * Time.deltaTime);
+       
+        myPlayer.Move(move * Time.deltaTime);
     }
 }
+
